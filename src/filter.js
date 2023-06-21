8a53b954b51370b87ba5a679ff24dfefa0aca910
songmin <tft0720@gmail.com>
2023-06-22 06:33:45 +0900

    이터러블프로토콜 : filter 함수 생성

---
+++ src/filter.js
@@ -0,0 +1,34 @@
+/* -- filter 함수 -- */
+
+const products = [
+  { name: 'test1', price: 1000 },
+  { name: 'test2', price: 2000 },
+  { name: 'test3', price: 3000 },
+];
+
+const filter = (fn, iterator) => {
+  const res = [];
+  for (const value of iterator) {
+    if (fn(value)) {
+      res.push(value);
+    }
+  }
+  return res;
+};
+
+console.log(filter((value) => value.price > 2000, products));
+console.log(filter((value) => value % 2, [1, 2, 3, 4]));
+console.log(
+  filter(
+    (value) => value % 2,
+    (function* () {
+      yield 1;
+      yield 2;
+      yield 3;
+      yield 4;
+      yield 5;
+    })()
+  )
+);
+
+products.filter((value) => value.price > 2000);
