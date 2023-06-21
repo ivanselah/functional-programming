b6fb710d1a771a9cc108e1c881fb058bd57d5cad
songmin <tft0720@gmail.com>
2023-06-22 06:20:38 +0900

    이터러블프로토콜 : map 함수 생성

---
+++ src/map.js
@@ -0,0 +1,42 @@
+/* -- map 함수 -- */
+
+const products = [
+  {
+    name: 'test1',
+    price: 10000,
+  },
+  {
+    name: 'test2',
+    price: 20000,
+  },
+];
+
+const map = (fn, iterator) => {
+  const res = [];
+  for (const value of iterator) {
+    res.push(fn(value));
+  }
+  return res;
+};
+
+const result = map((value) => value.price, products);
+console.log(result);
+
+// 이터러블 프로토콜을 따른 map 의 다형성
+// [].map 이 없을 때, 이터러블 프로토콜만 따르면 위와 같이 만들어서 사용가능
+
+const all = document.querySelectorAll('*');
+map((element) => element.nodeName, all);
+
+function* gen() {
+  yield 2;
+  yield 3;
+  yield 4;
+}
+console.log(map((value) => value * 2, gen())); // 4, 6, 8
+
+const mapValue = new Map(); // 이터러블 프로토콜을 따름
+mapValue.set('hello', 10);
+mapValue.set('world', 20);
+
+console.log(new Map(map(([key, value]) => [key, value * 2], mapValue))); // {'hello' -> 20, 'world' -> 40}
